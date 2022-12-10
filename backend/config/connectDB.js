535bf201932d2ac3fd3d607066f940840e1fa59c
Thanh Vinh <vuthanhvinh438@gmail.gmailcom>
2022-12-10 21:18:54 +0700

    #4

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    password: 'Vuthanhvinh438.',
+    password: process.env.DB_PASSWORD,
     user: 'root',
     database: 'tvshop',
     waitForConnections: true,
